482c59ec1ea40a905448a9bee47168f251f7809a
ARYANCY <aryan.crores@gmail.com>
2025-09-13 21:22:38 +0530

    changes

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,10 +1,11 @@
-
 module.exports = (req, res, next) => {
-
-  if (!req.session?.user) {
+  // Check the actual session key where user ID is stored
+  if (!req.session?.userId) {
     return res.status(401).json({ success: false, message: "Please log in first" });
   }
 
+  // Attach user object to request
   if (!req.user) req.user = { _id: req.session.userId };
+
   next();
 };
