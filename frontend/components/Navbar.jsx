e7857adafa27437e49418341b28d50b49d7db2b9
ARYANCY <aryan.crores@gmail.com>
2025-09-13 19:59:21 +0530

    changes

--- frontend/components/Navbar.jsx
+++ frontend/components/Navbar.jsx
@@ -1,16 +1,29 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import ReminderBell from "./ReminderBell";
 import "../css/Navbar.css";
 
 export default function Navbar({ user }) {
   return (
     <nav className="navbar">
+      {/* Top section */}
       <div className="navbar-top">
         <h1 className="navbar-title">Maitri</h1>
       </div>
+
+      {/* Bottom section */}
       <div className="navbar-bottom">
-        <div className="navbar-left"></div>
+        <div className="navbar-left">
+          {/* Feeling lonely button */}
+          <a
+            href="https://chat-app-ashen-phi.vercel.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="navbar-chat-btn"
+          >
+            Feeling lonely? Talk to or make a friend
+          </a>
+        </div>
+
         <div className="navbar-right">
           <ReminderBell />
           <span className="navbar-user">Hello, {user?.name || "Guest"}</span>
