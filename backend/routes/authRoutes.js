0cb0f8c4ab60e0049a6ff6a4e4dc1afb966e0379
ARYANCY <aryan.crores@gmail.com>
2025-10-11 04:55:57 +0530

    changes

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const passport = require("../config/passport");
-
+const User = require("../models/User");
 router.get(
   "/google",
   passport.authenticate("google", { scope: ["profile", "email"] })
