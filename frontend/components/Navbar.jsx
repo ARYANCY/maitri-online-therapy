7872e5651e7ad8bbc3d48be1549cc78c1634c884
ARYANCY <aryan.crores@gmail.com>
2025-10-10 20:44:28 +0530

    navbar css

--- frontend/components/Navbar.jsx
+++ frontend/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import ReminderBell from "./ReminderBell";
 import { useTranslation } from "react-i18next";
-import GULogo from "../src/images/gu-logo.svg";
+import GULogo from "../src/images/logo.png";
 import { Link, useNavigate } from "react-router-dom";
 import "../css/Navbar.css";
 
