d12ecd9d7e28917f91137af0eabaa9a9f26e4f62
ARYANCY <aryan.crores@gmail.com>
2025-10-11 03:52:54 +0530

    changes

--- backend/routes/therapistAdminRoutes.js
+++ backend/routes/therapistAdminRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { requireLogin, requireAdmin } = require("../../middleware/authMiddleware");
+const { requireLogin, requireAdmin } = require("../middleware/authMiddleware");
 const {
   getAllTherapists,
   updateTherapistStatus,
