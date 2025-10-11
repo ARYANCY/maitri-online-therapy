a24e553fed2480133a7b63701f4cf4fc8a8a692c
ARYANCY <aryan.crores@gmail.com>
2025-10-11 19:48:24 +0530

    changeS

--- backend/routes/therapistRoutes.js
+++ backend/routes/therapistRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { createTherapist, getAcceptedTherapists } = require("../controllers/therapistController");
 
-router.post("/apply", createTherapist);         // Anyone can apply
-router.get("/accepted", getAcceptedTherapists); // Anyone can view accepted therapists
+router.post("/apply", createTherapist); 
+router.get("/accepted", getAcceptedTherapists); 
 
 module.exports = router;
